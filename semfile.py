74bda39f593ac9680872f9b130ad49d7a18dd857
Drew Perttula <drewp@bigasterisk.com>
2005-07-06 22:54:14 -0700

    now displays files/tags with nevow
    darcs-hash:20050707055414-836b1-41a1ee1c90f95b89fcd65fa1f42a8079db2cedc0.gz

--- semfile.py
+++ semfile.py
@@ -1,7 +1,8 @@
 
-from rdflib import Graph
+from rdflib import Graph, RDF
 
 def read_or_create_graph(graph_filename):
+    # someday this will probably return a proxy to a shared, networked graph
     graph = Graph()
     try:
         graph.load(graph_filename)
@@ -10,3 +11,16 @@ def read_or_create_graph(graph_filename):
     return graph
 
 graph_filename = "tags.rdf"
+
+def all_documents(graph):
+    # this is hardly correct. what i want is all S with (S, RDF.type,
+    # foaf:Document); but including any other types that are
+    # subClassOf foaf:Document
+
+    ## [06:12:15] <eikeon> This is one way:
+    ## [06:12:16] <eikeon> allPeople = set()
+    ## [06:12:16] <eikeon> for personType in store.transitive_subjects(RDFS.subClassOf, TERROR.Person):
+    ## [06:12:16] <eikeon>     for person in store.subjects(RDF.type, personType):
+    ## [06:12:16] <eikeon>         allPeople.add(person)
+    
+    return graph.subjects(predicate=RDF.type)
